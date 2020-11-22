9fdecbe3a9e24e5e52bc3339094e718d6fc37ac5
Lwazi <lwaziradebe100@gmail.com>
2020-11-22 08:41:41 +0200

    Area of circle

---
+++ area_of_circle.py
@@ -0,0 +1,8 @@
+def area_of_circle(radius):
+    """This function takes the radius of circle and returns its Area"""
+
+    pie = 3.14
+    Area = pie * (radius**2)
+    print(f"The circle's Area is: {Area}")
+
+
