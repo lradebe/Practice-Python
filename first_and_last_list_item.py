d6fafa69c2f0270dc78db89e54ca0e2e36cf168c
Lwazi <lwaziradebe100@gmail.com>
2020-11-22 11:52:01 +0200

    First and last item in List

---
+++ first_and_last_list_item.py
@@ -0,0 +1,9 @@
+def first_and_last_list_item(List):
+    """This function should return the first and last item in List"""
+
+    print(f"This is the List: \n{List}")
+    print(f"\nThis is the first item in List: {List[0]}")
+    print(f"This is the last item in List: {List[-1]}")
+
+List = ["This", "is", "First", "and", "Last", "item", "in", "List"]
+first_and_last_list_item(List)
