01099f475fa74eaacb9ed09f8b3ec95d6489a2c2
Lwazi <lwaziradebe100@gmail.com>
2020-11-22 09:00:31 +0200

    Reverse Name and surname

---
+++ reverse_names.py
@@ -0,0 +1,7 @@
+def reverse(name, surname):
+    """This function reverses the name and surname of the user input with space in between"""
+
+    print(f"{name[::-1]} {surname[::-1]}")
+#[::-1] means it should start printing at the end of the string
+#and print all the way to position 0, using step -1 or one backwards or negative 1
+
